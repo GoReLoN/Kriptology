005f1c5c5f065dde3c38bc90d9681491f1239e0f
Дерек <@->
2019-05-24 14:00:32 +0300

    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ afinafin/Source.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int gcd(int, int);
+string& UpCase(string&);
+string& DelDouble(string&);
+
+int main()
+{
+	string alpha;
+	ifstream f_alpha("f_alpha.txt");
+	getline(f_alpha, alpha);
+	cout << DelDouble(UpCase(alpha)) << endl << alpha;//jkjkjk
+	return 0;
+}
+
+int gcd(int a, int b)
+{
+	while (a != 0 && b != 0)
+	{
+		if (a < b)
+			swap(a, b);
+		a %= b;
+	}
+	return (a + b);
+}
+
+string& UpCase(string& str)
+{
+	for (int i = 0; i < str.size(); ++i)
+		if (str[i] >= 97 && str[i] < 123)
+			str[i] -= 32;
+	return str;
+}
+
+string& DelDouble(string& str)
+{
+	string result = "";
+	for (int i = 0; i < str.size(); ++i)
+	{
+		bool found = false;
+		for (int j=0;j<result.size();++j)
+			if (str[i] == result[j])
+			{
+				found = true;
+				break;
+			}
+		if (!found)
+			result += str[i];
+	}
+	str = result;
+	return str;
+}
